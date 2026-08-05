75fa8d3530b99f168b21f85fe6ca161c9b2ed3c9
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-08-05 15:09:28 -0300

    adicionado a classe de musica e duas musicas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+musica1.nome = "Fogueira";
+musica1.artista = "Jorge e Mateus";
+
+Console.WriteLine($"Nome da música : {musica1.nome}");
+Console.WriteLine($"Artista da música : {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Duas Vidas";
+musica2.artista = "Henrique e Juliano";
