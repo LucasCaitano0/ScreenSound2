acccdf45fc2e7e78dfcb5e0bee9766a89f0f2648
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-09-23 14:43:27 -0300

    criando a nova classe Episodio

--- Program.cs
+++ Program.cs
@@ -1,27 +1,4 @@
-﻿Banda matue = new Banda("Matue");
-
-Album albumMatue = new Album("XTRANHO");
-
-Musica musica1 = new Musica(matue, "Anos Luz")
-{
-    Duracao = 320,
-    Disponivel = true
-};
-
-Musica musica2 = new Musica(matue, "Backstage")
-{
-    Duracao = 296,
-    Disponivel = false
-};
-
-albumMatue.AdicionarMusica(musica1);
-albumMatue.AdicionarMusica(musica2);
-matue.AdicionarAlbum(albumMatue);
-
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-
-albumMatue.ExibirMusicasDoAlbum();
-matue.ExibirDiscografia();
-
-
+﻿Episodio ep1 = new(1, "Introdfução ao C#", 30);
+ep1.AdicionarConvidado("Alice");
+ep1.AdicionarConvidado("Bob");
+Console.WriteLine(ep1.Resumo);
